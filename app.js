191ce08824940c0b5324ab0d70f338d4c252e46f
Vidhi Manglik <manglik.vidhi@gmail.com>
2018-10-04 18:47:32 +0000

    combined login signup frontend

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.get('/',function(req,res){
     res.render('about');
 });
 
-
+app.get('/login',function(req,res){
+    res.render('login');
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Server Started");
